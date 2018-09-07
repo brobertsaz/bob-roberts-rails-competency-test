d5b74824a1cbd862b9b58122b02c52a28dc9c0b4
Bob Roberts <cardsfanbob@gmail.com>
2018-09-07 16:31:05 -0500

    initial commit

---
+++ db/seeds.rb
@@ -0,0 +1,18 @@
+categories = %w[Tech Sport Lifestyle Politics]
+
+5.times do
+  User.create(
+    email: Faker::Internet.free_email,
+    password: 'Password1',
+    role: :editor
+  )
+end
+
+30.times do
+  Article.create(
+    user: User.all.sample,
+    category: categories.sample,
+    title: Faker::Lorem.sentence(4),
+    content: Faker::Lorem.paragraph(4)
+  )
+end
